c74c3be867e238dd06b87f12f0b7fb5f69d34b92
Wen Jufa <mjeffwhen@gmail.com>
2023-04-21 16:21:16 +0800

    Fix pack ignoring dirs

--- .github/workflows/pack.sh
+++ .github/workflows/pack.sh
@@ -21,14 +21,14 @@ which pbzip2 &> /dev/null || {
 echo "Save whole model-zoo..."
 tar -I pbzip2 \
     -cO \
-    --transform 's#^#model-zoo/#S' \
+    --transform 's#^./#model-zoo/#S' \
     --exclude ILSVRC2012_img_val/* \
     --exclude .git \
-    --exclude 'output/*' \
-    --exclude 'data/*' \
-    --exclude .github \
+    --exclude './output/*' \
+    --exclude './data/*' \
+    --exclude './.github' \
     --exclude __pycache__ \
-    --exclude 'venv' * | curl \
+    --exclude 'venv' ./* | curl \
     -T - $REMOTE/model-zoo_${revision}.tar.bz2
 
 [[ $top -ef ./ ]] || popd
